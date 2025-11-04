18b49623c2cbeb27ee1bfed6e0e2062bc5adbf3c
Nikolas Rocha <nikolas.rocha99@gmail.com>
2025-11-04 08:14:27 -0300

    Delete API directory

--- API/js-evaluations-api/src/controllers/healthController.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    healthCheck: (req, res) => {
-        res.status(200).json({ status: 'OK', message: 'API is healthy' });
-    }
-};
--- API/js-evaluations-api/src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        message: 'An unexpected error occurred.',
-        error: process.env.NODE_ENV === 'development' ? err : {}
-    });
-};
